538ef83170bd8b6f4ab51623f103de90a086cb7d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-05 11:14:09 +0300

    Wrote the prediction endpoint for our simple application

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 """The entrypoint to the application"""
 from fastapi import FastAPI
+from models.newsgroup_models import NewsgroupsModel, PredictionOutput
+from fastapi import Depends
 
 
 app = FastAPI()
+newsgroups_model = NewsgroupsModel()
 
 
 @app.get("/", name="Home", description="The root endpoint of the application", tags=["Home"])
@@ -13,3 +16,23 @@ async def home():
         dict[str, str]: A statement to show that the api works
     """
     return {"Hello": "World"}
+
+
+@app.post(
+    "/prediction",
+    name="Prediction",
+    description="Predict the newsgroups of a given text",
+    tags=["Prediction"]
+)
+async def prediction(
+    output: PredictionOutput = Depends(newsgroups_model.predict),
+):
+    """Predict the newsgroups of a given text
+
+    Args:
+        output (PredictionOutput): The output of the prediction
+
+    Returns:
+        PredictionOutput: The output of the prediction
+    """
+    return output
